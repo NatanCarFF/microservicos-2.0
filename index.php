a7a985e80f6062e0684329509550652db2488d36
Natanael Carvalho Da Silva <161735922+NatanCarFF@users.noreply.github.com>
2024-03-25 17:52:42 -0300

    Update index.php
    Atualização 250320241752

--- index.php
+++ index.php
@@ -14,9 +14,9 @@ header('Content-Type: text/html; charset=iso-8859-1');
 echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
 
 $servername = "54.234.153.24";
-$username = "root";
+$username = "natancarff";
 $password = "Senha123";
-$database = "meubanco";
+$database = "bancodedados";
 
 // Criar conexão
 
@@ -24,7 +24,7 @@ $database = "meubanco";
 $link = new mysqli($servername, $username, $password, $database);
 
 /* check connection */
-if (mysqli_connect_errno()) {
+if (mysqli_connect_error()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
@@ -34,11 +34,11 @@ $valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
 $host_name = gethostname();
 
 
-$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
+$query = "INSERT INTO dados (ClienteID, Nome, Sobrenome, Endereco, Cidade, Host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
 
 
 if ($link->query($query) === TRUE) {
-  echo "New record created successfully";
+  echo "Novo Registro incluido com sucesso!";
 } else {
   echo "Error: " . $link->error;
 }
